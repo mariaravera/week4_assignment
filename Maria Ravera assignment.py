5be859d494c7c382faf81dd2bfe756e55f320246
mariaravera <113367596+mariaravera@users.noreply.github.com>
2022-09-13 15:21:59 +0200

    Add files via upload

---
+++ Maria Ravera assignment.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Maria Ravera**
+# 
+# PhD student
+
+# *I am interested in data science because I would like to gain more technical knowledge on top of my theoretical and practical skills in the Lab.*
+
+# ### My code in Cell 5 will calculate the number of seconds in a day
+
+# In[6]:
+
+
+seconds_day=60*60*24
+print(seconds_day)
+
+
+# 1. There are 24 hours in a day
+# 2. There are 60 minutes in a hour
+# 3. There are 60 seconds in a minute
+# ___
+# | unit | seconds |
+# | --- | --- |
+# | second | 1 |
+# | minute | 60 |
+# | hour | 3600 |
+# | day | 86400 |
+# 
+# ___
+# [Measuring time](https://en.wikipedia.org/wiki/Time_in_physics#The_unit_of_measurement_of_time:_the_second)
+# ___
+
+# In[ ]:
+
+
+
+
